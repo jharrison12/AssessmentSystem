6ca2427068cc6a27af1063ca86527ea1cbc7e5fa
Jonathan Harrison <jharrison12@gmail.com>
2016-06-30 14:24:39 -0500

    Update home page view and add unit test to test http response

--- rubricapp/tests.py
+++ rubricapp/tests.py
@@ -1,3 +1,4 @@
+from django.http import HttpRequest
 from django.test import TestCase
 from rubricapp.views import home_page
 from django.core.urlresolvers import resolve
@@ -7,5 +8,15 @@ class HomePageTest(TestCase):
 	def test_home_url_resolves_to_home_page_view(self):
 		found = resolve('/')
 		self.assertEqual(found.func, home_page) 
+	
+	def test_home_page_returns_correct_html(self):
+		request = HttpRequest()
+		response = home_page(request)
+		self.assertTrue(response.content.startswith(b'<html>'))
+		self.assertIn(b'<title>Assessment System</title>', response.content)
+		self.assertTrue(response.content.endswith(b'</html>'))
+
+	#def test_home_page_contains_assessment_in_title(self):
+		
 # Create your tests here.
 	
--- rubricapp/views.py
+++ rubricapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-def home_page():
-	pass
+def home_page(request):
+	return HttpResponse('<html><title>Assessment System</title></html>')
+
 # Create your views here.
