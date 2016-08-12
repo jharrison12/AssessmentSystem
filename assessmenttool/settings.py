ac63f41be300369d68c55378f7aa680359782db7
Jonathan Harrison <jharrison12@gmail.com>
2016-08-12 16:26:16 -0500

    Register dataview app

--- assessmenttool/settings.py
+++ assessmenttool/settings.py
@@ -127,6 +127,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rubricapp',
+	'dataview',
     # Uncomment the next line to enable the admin:
      'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
--- dataview/views.py
+++ dataview/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 
 def home_page(request):
-	return render(request, 'dataviewhome.html', )
+	return render(request, 'dataview/dataviewhome.html', )
