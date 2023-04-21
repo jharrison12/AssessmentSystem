0a0ac8323a2d5e46b8f80981ad8fbf77fc7e5459
Jonathan Harrison <jharrison12@gmail.com>
2023-04-20 20:46:29 -0400

    Fix path for ^ url

--- assessmenttool/urls.py
+++ assessmenttool/urls.py
@@ -15,17 +15,17 @@ app_name = 'assessment'
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
-        re_path(r'^__debug__/', debug_toolbar.urls),
+        re_path(r'__debug__/', include('debug_toolbar.urls')),
     # Examples:
-	 re_path(r'^assessment/', include('rubricapp.urls'),namespace="rubricapp"),
-	 re_path(r'^data/', include('dataview.urls'),namespace="dataview"),
-	 re_path(r'^user/', views.user_page, name='userpage'),
+	    re_path(r'^assessment/', include('rubricapp.urls',namespace="rubricapp")),
+	    re_path(r'^data/', include('dataview.urls',namespace="dataview")),
+	    re_path(r'^user/', views.user_page, name='userpage'),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     # Uncomment the next line to enable the admin:
-    path('admin/', admin.site.urls),
+        path('admin/', admin.site.urls),
 	#url(r'^password_change/$', auth_views.password_change, {'password_change_form': PwordChangeForm}), 
-    re_path('^', 'django.contrib.auth.urls',),
+        re_path('^', include('django.contrib.auth.urls')),
 ]
 else:
     urlpatterns = [
