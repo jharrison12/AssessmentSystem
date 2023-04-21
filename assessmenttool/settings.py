57b4cbe15c21c2eb581566d1fb26281da6b525f2
Jonathan Harrison <jharrison12@gmail.com>
2023-04-20 20:46:03 -0400

    Update settings to reflect new django debug toolbar

--- assessmenttool/settings.py
+++ assessmenttool/settings.py
@@ -139,7 +139,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
-    #'debug_toolbar',
+    'debug_toolbar',
 
 )
 
